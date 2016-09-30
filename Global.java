b7ca4c28e647f30d6e7b34435f49cab473514252
Joshua Wright <Joshuawright1197@gmail.com>
2016-09-29 22:30:07 -0400

    Delete Global.java

--- Global.java
+++
@@ -1,9 +0,0 @@
-package Store;
-
-import java.util.ArrayList;
-
-public class Global {
-	static public double subtotal = 0;
-	static public Customer customer;
-	static public ArrayList<Purchase> cart;
-}
